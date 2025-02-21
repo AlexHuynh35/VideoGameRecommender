8d0c486e385e4503c6a72b438877fcda2ebe6f8b
Alex Huynh <alexh3925@gmail.com>
2025-02-20 21:48:22 -0800

    added platforms and companies

--- recommenderBackend/utils/appHelperFunc.py
+++ recommenderBackend/utils/appHelperFunc.py
@@ -3,6 +3,7 @@ def retrieve_game_info(cursor, numGames):
     cursor.execute(game_info_query, (numGames,))
     games = cursor.fetchall()
     game_id_list = [game[0] for game in games]
+
     game_genre_query = """
     SELECT games.id AS game_id, ARRAY_AGG(genres.name) AS genres
     FROM games
@@ -14,10 +15,37 @@ def retrieve_game_info(cursor, numGames):
     cursor.execute(game_genre_query, (game_id_list,))
     genres = cursor.fetchall()
     genre_dict = {row[0]: row[1] for row in genres}
+
+    game_platform_query = """
+    SELECT games.id AS game_id, ARRAY_AGG(platforms.name) AS platforms
+    FROM games
+    JOIN game_platforms ON games.id = game_platforms.game_id
+    JOIN platforms ON game_platforms.platform_id = platforms.id
+    WHERE games.id = ANY(%s)
+    GROUP BY games.id, games.name;
+    """
+    cursor.execute(game_platform_query, (game_id_list,))
+    platforms = cursor.fetchall()
+    platform_dict = {row[0]: row[1] for row in platforms}
+
+    game_company_query = """
+    SELECT games.id AS game_id, ARRAY_AGG(companies.name) AS companies
+    FROM games
+    JOIN game_companies ON games.id = game_companies.game_id
+    JOIN companies ON game_companies.company_id = companies.id
+    WHERE games.id = ANY(%s)
+    GROUP BY games.id, games.name;
+    """
+    cursor.execute(game_company_query, (game_id_list,))
+    companies = cursor.fetchall()
+    company_dict = {row[0]: row[1] for row in companies}
+
     games_with_all_info = []
     for game in games:
         game_genres = genre_dict.get(game[0], [])
-        games_with_all_info.append(game + (game_genres,))
+        game_platforms = platform_dict.get(game[0], [])
+        game_companies = company_dict.get(game[0], [])
+        games_with_all_info.append(game + (game_genres, game_platforms, game_companies))
     return games_with_all_info
 
 def readable_game_list(games):
@@ -29,6 +57,8 @@ def readable_game_list(games):
             "first_release_date": game[2], 
             "rating": game[3], 
             "cover_url": game[4],
-            "genre": game[5]
+            "genres": game[5],
+            "platforms": game[6],
+            "companies": game[7]
         })
     return game_list
