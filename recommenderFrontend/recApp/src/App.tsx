b63b30edc7f3dc5822198c9b8f2a0e42d5f871c5
Alex Huynh <alexh3925@gmail.com>
2025-03-15 02:01:19 -0700

    started styling app

--- recommenderFrontend/recApp/src/App.tsx
+++ recommenderFrontend/recApp/src/App.tsx
@@ -35,25 +35,45 @@ function App() {
   if (error) return <p>Error: {error}</p>;
 
   return (
-    <div>
-      <h1>Game List</h1>
-      <ul>
-        {games.map((game) => {
-          return (
-            <div>
-              <li key={game.id}>
-                <img src={game.cover_url} alt={game.name} width="100" />
-                <p>
-                  <strong>{game.name}</strong> - Rating: {game.rating} - Released: {game.first_release_date}
-                </p>
-              </li>
-              <BulletList listType="genres" list={game.genres} />
-              <BulletList listType="platforms" list={game.platforms} />
-              <BulletList listType="companies" list={game.companies} />
-            </div>
-          );
-        })}
-      </ul>
+    <div className="flex flex-col justify-center items-center">
+      <div>
+        <h1>Game List</h1>
+      </div>
+      <div>
+        <div className="grid grid-cols-1 gap-4">
+          {games.map((game) => {
+            return (
+              <div className="flex bg-blue-950 rounded-xl outline-solid outline-4 outline-black p-4" key={game.id}>
+                <div className="flex justify-center w-50 flex-none">
+                  <div>
+                    <img src={game.cover_url} alt={game.name} width="150" />
+                  </div>
+                </div>
+                <div className="w-100 flex-none">
+                  <div className="flex">
+                    <div className="w-80 flex-none">
+                      <div>
+                        <p><strong>{game.name}</strong></p>
+                      </div>
+                      <div>
+                        <p>Released: {game.first_release_date}</p>
+                      </div>
+                    </div>
+                    <div className="w-20 flex-none">
+                      <p><strong>{game.rating}</strong></p>
+                    </div>
+                  </div>
+                  <div className="grid grid-cols-3 gap-3">
+                    <BulletList listType="Genres" list={game.genres} />
+                    <BulletList listType="Platforms" list={game.platforms} />
+                    <BulletList listType="Companies" list={game.companies} />
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
--- recommenderFrontend/recApp/src/utilsComp/BulletList.tsx
+++ recommenderFrontend/recApp/src/utilsComp/BulletList.tsx
@@ -7,17 +7,25 @@ type ListProp = {
 
 const BulletList: React.FC<ListProp> = ({ listType, list }) => {
   if (!list || list.length === 0) {
-    return <p>No {listType} available.</p>;
+    return (
+      <div>
+        <p>{listType}</p>
+        <p>No {listType} available!</p>
+      </div>
+    );
   }
     
   return (
-    <ul>
-      {list.map((item, index) => (
-        <li key={index}>
-          {item}
-        </li>
-      ))}
-    </ul>
+    <div>
+      <p>{listType}</p>
+      <ul>
+        {list.map((item, index) => (
+          <li key={index}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
