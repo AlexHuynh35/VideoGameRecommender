e3b98d24c20cfa52df18121f1095b8dc7808bfe2
Alex Huynh <alexh3925@gmail.com>
2025-10-19 21:51:32 -0700

    chore: switched to next.js

--- recommenderFrontend/recApp/src/services/api.ts
+++
@@ -1,9 +0,0 @@
-export const API_URL = "http://127.0.0.1:5000";
-
-export async function fetchGames() {
-  const response = await fetch(`${API_URL}/test_return_filtered_game_list`);
-  if (!response.ok) {
-    throw new Error("Failed to fetch games");
-  }
-  return response.json();
-}
